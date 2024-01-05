c1265b845f43c60cd1f9f5d64cc698a70e8de0d3
as6325400 <as6325400@gmail.com>
2024-01-05 20:09:42 +0800

    test codebook

--- codebook/6_Math/floor_enumeration.cpp
+++
@@ -1,5 +0,0 @@
-// enumerating x = floor(n / i), [l, r]
-for (int l = 1, r; l <= n; l = r + 1) {
-  int x = n / l;
-  r = n / x;
-}
--- codebook/9_Else/BitsetLCS.cpp
+++
@@ -1,9 +0,0 @@
-cin >> n >> m;
-for (int i = 1, x; i <= n; ++i)
-  cin >> x, p[x].set(i);
-for (int i = 1, x; i <= m; i++) {
-  cin >> x, (g = f) |= p[x];
-  f.shiftLeftByOne(), f.set(0);
-  ((f = g - f) ^= g) &= g;
-}
-cout << f.count() << '\n';
