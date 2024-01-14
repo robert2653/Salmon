43cbc5a49cbc629fe9f1237bfe5c0829f8be49a8
robert2653 <robert4812653@gmail.com>
2024-01-14 12:13:34 +0800

    update kmp

--- codebook/String/_KMP.cpp
+++ codebook/String/_KMP.cpp
@@ -1,36 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct KMP {
-    string s;
+    string sub;
     vector<int> failure;
-    KMP(string s) {
-        this->s = s;
-        failure.resize(s.size(), -1);
+    KMP(string &sub) {
+        this->sub = sub;
+        failure.resize(sub.size(), -1);
+        buildFailFunction();
     }
     void buildFailFunction() {
-        for(int i = 1; i < s.size(); i++) {
+        for(int i = 1; i < sub.size(); i++) {
             int now = failure[i - 1];
-            while(now != -1 && s[now + 1] != s[i]) now = failure[now];
-            if (s[now + 1] == s[i]) failure[i] = now + 1;
+            while(now != -1 && sub[now + 1] != sub[i]) now = failure[now];
+            if (sub[now + 1] == sub[i]) failure[i] = now + 1;
         }
     }
-    void KMPmatching(string &sub) {
+    vector<int> KMPmatching(string &s) {
+        vector<int> match;
         for(int i = 0, now = -1; i < s.size(); i++) {
             // now is the compare sucessed length -1
             while (s[i] != sub[now + 1] && now != -1) now = failure[now];
             // f stores if comparison fail, move to where
             if (s[i] == sub[now + 1]) now++;
             if (now + 1 == sub.size()) {
-                cout << "found a match start at position " << i - now << "\n";
+                match.push_back(i - now);
                 now = failure[now];
             }
         }
+        return match;
     }
 };
 int main(){
-    string s = "BABA";
-    string sub = "BA";
-    KMP kmp(s);
-    kmp.buildFailFunction();
-    kmp.KMPmatching(sub);
+    string s = "xxtxxtxtx";
+    string sub = "tx";
+    KMP kmp(sub);
+    vector<int> ans = kmp.KMPmatching(s);
+    for(auto &i : ans) cout << i << " ";
 }
