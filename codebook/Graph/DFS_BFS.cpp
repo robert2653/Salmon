1d5ec5c3102966c9c7787a9d9d02b6a06485167d
robert2653 <robert4812653@gmail.com>
2024-02-27 21:27:57 +0800

    update dfs_bfs

--- codebook/Graph/DFS_BFS.cpp
+++ codebook/Graph/DFS_BFS.cpp
@@ -1,28 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6;
-vector<int> adj[maxn];
-bool vis[maxn];
-void DFS(int s){
-    if(vis[s]) return;
-    vis[s] = true;
-    for(auto u: adj[s]){
-        DFS(u);
-    }
-}
-queue<int> q;
-int dis[maxn];
-void BFS(int x){
-    vis[x] = true;
-    dis[x] = 0;
-    q.push(x);
-    while(!q.empty()){
-        int now = q.front();q.pop();
-        for(auto nxt : adj[now]){
-            if(vis[nxt]) continue;
-            vis[nxt] = true;
-            dis[nxt] = dis[now] + 1;
-            q.push(nxt);
+int main() {
+    int n;
+    vector<vector<int>> adj(n + 1, vector<int>());
+
+    // dfs_tree
+    auto dfs_tree = [&](auto self, int now, int par) -> void {
+        for (auto &nxt : adj[now]) {
+            if (nxt == par) continue;
+            self(self, nxt, now);
         }
-    }
+    };
+    dfs_tree(dfs_tree, 1, -1);
+
+    // dfs_graph
+    vector<bool> vis(n + 1, 0);
+    auto dfs = [&](auto self, int u) -> void {
+        if(vis[u]) return;
+        vis[u] = true;
+        for(auto v: adj[u]){
+            if(vis[v]) continue;
+            self(self, v);
+        }
+    };
+    dfs(dfs, 1);
+
+    // bfs
+    vector<int> deep(n + 1, 1e9);
+    queue<int> q;
+    auto bfs = [&](auto self, int u) -> void {
+        vis[u] = true;
+        deep[u] = 0;
+        q.push(u);
+        while(!q.empty()){
+            int now = q.front(); q.pop();
+            for(auto nxt : adj[now]){
+                if(vis[nxt]) continue;
+                vis[nxt] = true;
+                deep[nxt] = deep[now] + 1;
+                q.push(nxt);
+            }
+        }
+    };
+    bfs(bfs, 1);
 }
