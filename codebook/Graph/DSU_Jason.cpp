9033a712d26349c619029e38b100ee7a119b12a3
Jason0411202 <Jason0411202@gmail.com>
2024-03-06 16:32:32 +0800

    update format

--- codebook/Graph/DSU_Jason.cpp
+++ codebook/Graph/DSU_Jason.cpp
@@ -31,28 +31,3 @@ struct DSU {
         return siz[find_boss(x)];
     }
 };
-
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-
-    llt n=4;
-    vector<pair<llt, llt>> m={{1, 2}, {3, 4}};
-
-    DSU dsu(n); // 宣告一個 dsu 物件，大小為 n (1~n)
-
-    dsu.merge(1, 2); // 合併點 1 所在的集合與點 2 所在的集合
-    dsu.merge(3, 4); // 合併點 3 所在的集合與點 4 所在的集合
-
-    cout<<dsu.find_boss(1)<<endl; //輸出點 1 的 parent 是誰 (輸出: 1)
-
-    cout<<dsu.same(1, 3)<<endl; // 確認點 1 與 點 3 的 parent 是否相同 (輸出: 0)
-
-    cout<<dsu.size(1)<<endl; // 取得點 1所在的集合大小 (輸出: 2)
-
-    for(auto x: dsu.boss) cout<<x<<" "; //直接存取 boss 陣列，(輸出: 0 1 1 3 3)
-    cout<<endl;
-
-    return 0;
-}
