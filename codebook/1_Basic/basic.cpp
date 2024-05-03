f146308df08e29eec5c3263007defd74dd0300ce
robert2653 <robert4812653@gmail.com>
2024-05-04 07:29:52 +0800

    basic op

--- codebook/1_Basic/basic.cpp
+++ codebook/1_Basic/basic.cpp
@@ -31,6 +31,18 @@ struct cmp {    // 在有 template 的資結使用
     // 內建 multiset 可以跟 set 一樣正常使用, 自定義比較結構就比照以上
 };
 
+struct cmp {
+    vector<int> &v;
+    cmp(vector<int>& vec) : v(vec) {}
+    bool operator() (int a, int b) const {
+        // 根據外部向量來比較元素的優先級，記得不要改到比較 vector
+        return v[a] > v[b];
+    }
+};
+// main:
+// cmp cmp1(vector);
+// priority_queue<int, vector<int>, cmp> pq(cmp1);
+
 const int llinf = 4e18;
 const int inf = 2e9;
 const int mod = 1e9 + 7;
