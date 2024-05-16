b087a23bf7ee0ad1fe8f1d37208f84691953416c
robert2653 <robert4812653@gmail.com>
2024-05-16 18:42:01 +0800

    cmp

--- codebook/1_Basic/cmp.cpp
+++ codebook/1_Basic/cmp.cpp
@@ -6,15 +6,16 @@ struct cmp {    // 在有 template 的資結使用
     // priority queue 小到大是 > , set 是 <
     // set 不能 = , multiset 要 = 
     // 每個元素都要比到，不然會不見
-    // pbds_multiset 的 upper_bound 跟 lower_bound 功能相反, 如果要 find, 插入 inf 後使用 upper_bound
-    // 內建 multiset 可以跟 set 一樣正常使用, 自定義比較結構就比照以上
+    // pbds_multiset 不要用 lower_bound
+    // 如果要 find, 插入 inf 後使用 upper_bound
+    // 內建 multiset 可以跟 set 一樣正常使用
+    // 如果有自定義比較結構就比照以上
 };
 
 struct cmp {
     vector<int> &v;
     cmp(vector<int>& vec) : v(vec) {}
     bool operator() (int a, int b) const {
-        // 根據外部向量來比較元素的優先級，記得不要改到比較 vector
         return v[a] > v[b];
     } 
 // main: cmp cmp1(vector);
