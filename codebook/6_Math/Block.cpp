7862d7d5801174aee69fffe13d04293b7dfe71b5
robert2653 <robert4812653@gmail.com>
2024-05-16 22:29:06 +0800

    .

--- codebook/6_Math/Block.cpp
+++ codebook/6_Math/Block.cpp
@@ -3,9 +3,6 @@ const int mod = 1e9 + 7;
 const int inv_2 = 500000004;
 // n / 1 * 1 + n / 2 * 2 + n / 3 * 3 + ... + n / n * n
 signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int ans = 0;
     int n; cin >> n;
     for (int l = 1, r; l <= n; l = r + 1) {
