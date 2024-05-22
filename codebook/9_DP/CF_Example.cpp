5a0cf0b9787454284bb65e8c16b1f8da7e1f521d
robert2653 <robert4812653@gmail.com>
2024-05-23 07:14:20 +0800

    Add CF Messenger in MAC

---
+++ codebook/9_DP/CF_Example.cpp
@@ -0,0 +1,62 @@
+// CF 1932 pF
+// 給你很多區間，你可以選一些點，重疊到的線段得到 1 分
+// 請問在線段不重複的情況下，最多獲得幾分
+int main() {
+    int n, m;
+    cin >> n >> m;
+    // 記錄每點有幾個線段
+    // 再一個紀錄，包含這個點的左界
+    vector<int> l_side(n + 1, inf), cnt(n + 5, 0);
+    for (int i = 0; i < m; i++) {
+        int l, r; cin >> l >> r;
+        l_side[r] = min(l_side[r], l);
+        cnt[l]++;
+        cnt[r + 1]--;
+    }
+    for (int i = 2; i <= n; i++) {
+        cnt[i] += cnt[i - 1];
+    }
+    for (int i = n; i >= 2; i--) {
+        l_side[i - 1] = min(l_side[i - 1], l_side[i]);
+    }
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = cnt[i];
+        if (l_side[i] != inf) {
+            dp[i] += dp[l_side[i] - 1];
+        }
+        dp[i] = max(dp[i], dp[i - 1]);
+    }
+    cout << dp[n] << "\n";
+}
+
+// CF 1935 pC
+// 給你每個事件的 a, b，挑事件會把 a 全部加起來
+// 再加上 max(bi) - min(bi)
+int main(){
+    int n, k, ans = 0; cin >> n >> k;
+    vector<pii> v(n + 1);
+    for (int i = 1; i <= n; i++) {
+        int a, b; cin >> a >> b;
+        v[i] = {a, b};
+        if (a <= k) ans = 1;
+    }
+    sort(v.begin() + 1, v.end(), [](pii &a, pii &b) {
+        return a.second < b.second;
+    }); // 用 bi 來排，考慮第 i 個時可以先扣
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, inf));
+    // 考慮 v[i] 時, 選 j 個的 sum(ai) - min(bi)
+    for (int i = 1; i <= n; i++) { // 滾動 dp
+        for (int j = n; j >= 2; j--) {
+            dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1] + v[i].first);
+            // min(不選, 選)
+            if (dp[i - 1][j - 1] + v[i].first + v[i].second <= k) {
+                // 假如可以選，更新 ans 時再加回去 bi
+                ans = max(ans, j);
+            }
+        }
+        dp[i][1] = min(dp[i - 1][1], v[i].first - v[i].second);
+    }
+    cout << ans << endl;
+}
--- codebook/9_DP/Projects.cpp
+++ codebook/9_DP/Projects.cpp
@@ -1,3 +1,4 @@
+// 排程有權重問題，輸出價值最多且時間最少
 struct project {
     int from, end, gain, id;
 };
@@ -5,34 +6,33 @@ int main() {
     int n; cin >> n;
     vector<project> projects(n + 1);
     for (int i = 1; i <= n; i++) {
-        cin >> projects[i].from >> projects[i].end >> projects[i].gain;
-        projects[i].id = i;
+        int f, e, g; cin >> f >> e >> g;
+        projects[i] = {f, e, g, i};
     }
     sort(all(projects), [](project a, project b) {
         if (a.end == b.end) return a.gain < b.gain;
         return a.end < b.end;
     });
-    vector<array<int, 3>> dp(n + 1, {0, 0, 0}); // nums, gain, time
+    vector<array<int, 3>> dp(n + 1); // nums, gain, time
     vector<int> par(n + 1, 0), ans, add(n + 1, -1);
     for (int i = 1; i <= n; i++) {
-        int idx = --upper_bound(projects.begin(), projects.begin() + i, project({0, projects[i].from, 0, 0}),
-            [](const project &a, const project &b) -> bool {
-                return a.end < b.end;
-            }) - projects.begin();   // 二分搜最接近 from 的 end
+        int id = --upper_bound(projects.begin(), projects.begin() + i, project({0, projects[i].from, 0, 0}), [](project &a, project &b) {
+            return a.end < b.end;
+        }) - projects.begin();   // 二分搜最接近 from 的 end
         dp[i] = dp[i - 1];
         par[i] = i - 1;
-        if (dp[i][1] < dp[idx][1] + projects[i].gain || 
-        (dp[i][1] == dp[idx][1] + projects[i].gain && dp[i][2] > dp[idx][2] + projects[i].end - projects[i].from)) {
-            dp[i] = {dp[idx][0] + 1, dp[idx][1] + projects[i].gain, dp[idx][2] + projects[i].end - projects[i].from};
-            par[i] = idx;
+        if (dp[i][1] < dp[id][1] + projects[i].gain || (dp[i][1] == dp[id][1] + projects[i].gain && dp[i][2] > dp[id][2] - projects[i].from + projects[i].end)) {
+            // 如果報酬率一樣，比時間少的
+            dp[i] = {dp[id][0] + 1, dp[id][1] + projects[i].gain, dp[id][2] + projects[i].end - projects[i].from};
+            par[i] = id;
             add[i] = projects[i].id;
         }
     }
-    cout << dp[n][0] << " " << dp[n][1] << " " << dp[n][2] << "\n";
-    for (int now = n; now > 0; now = par[now]) {
+    for (auto i : dp[n])
+        cout << i << " " << "\n";
+    for (int now = n; now > 0; now = par[now])
         if (add[now] != -1)
             ans.push_back(add[now]);
-    }
     sort(all(ans));
     for (auto &i : ans) cout << i << " ";
 }
