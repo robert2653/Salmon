6ac88ac11d974d1801c633fd35c952a0478e4eac
robert2653 <robert4812653@gmail.com>
2024-06-30 12:26:58 +0800

    update prim

--- codebook/2_Graph/Prim.cpp
+++ codebook/2_Graph/Prim.cpp
@@ -1,32 +1,19 @@
-int n, m, ans = 0;
-const int maxn = 2e5 + 5;
-vector adj(maxn, vector<pair<int, int>>());
-bool Prim() {
+auto prim = [&](int n, vector<vector<pair<int, int>>> &adj) -> bool {
     int node_sz = 0;
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push({0, 1}); // w, vertex
-    vector<bool> vis(maxn);
+    vector<bool> vis(n);
     while (!pq.empty()) {
-        auto [cost, u] = pq.top(); pq.pop();
+        auto [u, w] = pq.top(); pq.pop();
         if (vis[u]) continue;
         vis[u] = true;
-        ans += cost;
         node_sz++;
-        for (auto [v, cost] : adj[u]) {
-            if (!vis[v])
-                pq.push({cost, v});
+        for (auto v : adj[u]) {
+            if (!vis[v.first]) {
+                pq.push({v.second, v.first});
+            }
         }
     }
     if (node_sz == n) return true;
     return false;
-}
-void solve() {
-    cin >> n >> m;
-    for(int i = 1; i <= m; i++) {
-        int u, v, cost; cin >> u >> v >> cost;
-        adj[u].push_back({v, cost});
-        adj[v].push_back({u, cost});
-    }
-    if (Prim()) cout << ans;
-    else cout << "IMPOSSIBLE";
-}
+};
