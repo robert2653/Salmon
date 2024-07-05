28eaefa1b728b12a06147b368e0ca6af5eccd4a0
robert2653 <robert4812653@gmail.com>
2024-07-05 10:19:39 +0800

    update min_cut

--- codebook/4_Flow/Min_Cut.cpp
+++ codebook/4_Flow/Min_Cut.cpp
@@ -1,32 +1,37 @@
 // CSES Police Chase
-int g[505][505]; // 以 O(1) 紀錄存在邊
-void solve(){
-    cin >> n >> m;
+void solve(){int n, m; cin >> n >> m;
+    Dinic<int> g(n);
     for (int i = 0; i < m; i++) {
-        int u, v; cin >> u >> v;
-        add_edge(u, v, 1);
+        int u, v, cap = 1;
+        cin >> u >> v;
+        u--; v--;
+        g.add_edge(u, v, cap);
+        g.add_edge(v, u, cap);
     }
-    dinic();
-    fill(all(vis), 0);
-    unordered_set<int> reach;
+    int res = g.work(0, n - 1);
+    cout << res << "\n";
+    if (res == 0) return;
+
+    vector<int> vis(n);
     auto find = [&](auto self, int u) -> void {
         if (!vis[u]) {
             vis[u] = 1;
-            reach.insert(u);
-            for (auto [v, w, _] : adj[u]){
-                if(w > 0){
-                    self(self, v);
+            for (int id : g.adj[u]) {
+                auto e = g.edges[id];
+                if (e.cap - e.flow > 0) {
+                    self(self, e.to);
                 }
             }
         }
     };
-    find(find, 1);
-    cout << ans << "\n";
-    for (auto u : reach) {
-        for (auto [v, w, _] : adj[u]) {
-            if (g[u][v] && !w && reach.find(v) == reach.end()) {
-                cout << u << " " << v << "\n";
-                // ans = sum(u_to_v)
+    find(find, 0);
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) continue;
+        for (int id : g.adj[i]) {
+            if (id & 1) continue;
+            auto e = g.edges[id];
+            if (!vis[e.to]) {
+                cout << i + 1 << " " << e.to + 1 << "\n";
             }
         }
     }
