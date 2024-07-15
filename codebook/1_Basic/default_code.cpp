ab713b19d4a7989cee40297ae30bb5269db55d8a
robert2653 <robert4812653@gmail.com>
2024-07-15 17:06:21 +0800

    update default code

--- codebook/1_Basic/default_code.cpp
+++ codebook/1_Basic/default_code.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("03")
+#pragma GCC target("popcnt")
 #define all(x) (x).begin(), (x).end()
 #define pii pair<int, int>
 using namespace std;
 using ll = long long;
-const int mod = 1e9 + 7;
 
 void solve() {
 
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
     cin >> t;
