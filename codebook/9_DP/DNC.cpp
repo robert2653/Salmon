c0ac706580275b4316340e9e7bf731dd996d8d1b
robert2653 <robert4812653@gmail.com>
2024-07-18 18:39:46 +0800

    update DNC

--- codebook/9_DP/DNC.cpp
+++ codebook/9_DP/DNC.cpp
@@ -1,14 +1,16 @@
 constexpr int N = 3e3 + 5;
 constexpr ll inf = 4e18;
-// dp[k][j] = min(dp[k - 1][i - 1] + cost[i][j]);
-ll cost[N][N], dp[N][N]; // 1-based
+// dp[k][j] = min(dp[k - 1][i] + cost[i][j])
+// cost: (i, j]
+ll dp[N][N]; // 1-based
 ll get_cost(int l, int r) {}
 void DNC(int k, int l, int r, int optl, int optr) {
     if (l > r) return;
     int m = (l + r) >> 1, opt = -1;
     dp[k][m] = inf;
     for (int i = max(k, optl); i <= min(m, optr); i++) {
-        ll cur = dp[k - 1][i - 1] + get_cost(i, m);
+        // 注意 i 的範圍、 get_cost 與 dp 的邊界
+        ll cur = dp[k - 1][i] + get_cost(i, m);
         if (cur < dp[k][m]) {
             dp[k][m] = cur, opt = i;
         }
@@ -19,7 +21,7 @@ void DNC(int k, int l, int r, int optl, int optr) {
 int main() {
     // first build cost...
     for (int i = 1; i <= n; i++) {
-        dp[1][i] = get_cost(1, i);
+        // init dp[1][i]
     }
     for (int i = 2; i <= k; i++) {
         DNC(i, 1, n, 1, n);
