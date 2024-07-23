cbf6995aa73f31217bc0cffe76fc92db84ad1950
robert2653 <robert4812653@gmail.com>
2024-07-23 17:47:29 +0800

    Update cmp

--- codebook/1_Basic/cmp.cpp
+++ codebook/1_Basic/cmp.cpp
@@ -1,22 +1,14 @@
-struct cmp {    // 在有 template 的資結使用
-    bool operator()(const int &a, const int &b) const {
-        return a < b;
-    }
-// sort, bound 不用 struct
-// priority queue 小到大是 > , set 是 <
-// set 不能 = , multiset 要 = 
-// 每個元素都要比到，不然會不見
-// pbds_multiset 不要用 lower_bound
-// 如果要 find, 插入 inf 後使用 upper_bound
-// 內建 multiset 可以跟 set 一樣正常使用
-// 如果有自定義比較結構就比照以上
-};
-struct cmp {    // 要在 template 的資結用外部變數
-    vector<int> &v;
-    cmp(vector<int>& vec) : v(vec) {}
-    bool operator() (int a, int b) const {
-        return v[a] > v[b];
-    } 
-// main: cmp cmp1(vector);
-// priority_queue<int, vector<int>, cmp> pq(cmp1);
-};
+1. sort, 二分搜刻在函式內 lambda 就好
+2. priority queue 小到大是 >, set 是 <
+3. set 不能 = , multiset 必須 = 
+4. 確保每個成員都要比到
+5. pbds_multiset 不要用 lower_bound
+6. 如果要用 find, 插入 inf 後使用 upper_bound
+7. multiset 可以跟 set 一樣使用, 但請注意第 3、4 點
+
+auto cmp = [](int i, int j) { return i > j; };
+priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+
+vector<int> a {1, 2, 5, 4, 3}; // 小心不要改到 a
+auto cmp = [&a](int i, int j) { return a[i] > a[j]; };
+priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
