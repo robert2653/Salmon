3ac88905ad65ae137fd9308e0024608e6821af0b
robert2653 <robert4812653@gmail.com>
2024-07-27 02:26:51 +0800

    Add Rng

---
+++ codebook/1_Basic/Rng.cpp
@@ -0,0 +1,2 @@
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll x = rng();
--- codebook/8_Tree/Tree_Centroid.cpp
+++ codebook/8_Tree/Tree_Centroid.cpp
@@ -1,10 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
 struct CenDecom {
     int n;
     vector<vector<int>> adj;
     vector<bool> vis;
     vector<int> siz;
-    CenDecom() {}
-    CenDecom(int n_) { init(n_); }
+    CenDecom(int n_ = 0) { init(n_); }
     void init(int n_) {
         n = n_;
         adj.assign(n, {});
@@ -15,11 +16,11 @@ struct CenDecom {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    void get_siz(int dep, int x, int p = -1) {
+    void get_siz(int x, int p = -1) {
         siz[x] = 1;
         for (int y : adj[x]) {
             if (y == p || vis[y]) continue;
-            get_siz(dep + 1, y, x);
+            get_siz(y, x);
             siz[x] += siz[y];
         }
     }
@@ -32,11 +33,21 @@ struct CenDecom {
         }
         return x;
     }
+    void get_ans(int x, int p) {
+        // do something
+        for (int y : adj[x]) {
+            if (y == p || vis[y]) continue;
+            get_ans(y, x);
+        }
+    }
     void work(int x = 0) {
         get_siz(0, x);
         int cen = get_cen(x, siz[x]);
         vis[cen] = true;
-        // do something
+        for (int y : adj[cen]) {
+            if (vis[y]) continue;
+            get_ans(y, cen);
+        }
         for (int y : adj[cen]) {
             if (vis[y]) continue;
             work(y);
