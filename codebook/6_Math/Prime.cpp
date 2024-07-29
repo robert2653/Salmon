02f203f760603ed13774e79dd57891dc26648d20
robert2653 <robert4812653@gmail.com>
2024-07-30 03:20:15 +0800

    Fix var name

--- codebook/6_Math/Prime.cpp
+++ codebook/6_Math/Prime.cpp
@@ -1,31 +1,20 @@
-// a^(m-1) = 1 (mod m)
-// a^(m-2) = 1/a (mod m)
-// EXP2: cout << fast_exp(x, fast_exp(y, p, MOD - 1), MOD)
-// Filter + DP; DP save min factor，recur，factor decomposition
-// FacNums = (x+1)(y+1)(z+1)...
-// FacSum = (a^0+a^1...+a^x)(b^0+...+b^y)
-// FacMul = N(x+1)(y+1)(z+1)/2
-vector<int> is_prime;
-// 1 代表是質數，非 1 不是
-void init(int n) {
-    is_prime.assign(n + 1, 1);
-    for (int i = 2; i <= (int)sqrt(n) + 1; i++) {
-        if (is_prime[i] == 1) {
+vector<int> prime, minp;
+void sieve(int n) {
+    minp.assign(n + 1, 1); // 1 代表是質數，非 1 不是
+    minp[0] = minp[1] = -1;
+    int m = int(sqrt(n)) + 1;
+    for (int i = 2; i <= m; i++) {
+        if (minp[i] == 1) {
+            prime.push_back(i);
             for (int j = i + i; j <= n; j += i) {
-                is_prime[j] = i;
+                minp[j] = i;
             }
         }
     }
 }
-int main() {
-    init(1000000);
-    ll ans = 1, q; cin >> q;
-    map<ll, ll> mp;
-    while (is_prime[q] != 1) {
-        mp[is_prime[q]]++;
-        q /= is_prime[q];
-    }
-    if (q != 1) mp[q]++;
-    for (auto [a, b] : mp) ans *= b + 1;
-    cout << ans << "\n";
-}
+// a^(m-1) = 1 (mod m)
+// a^(m-2) = 1/a (mod m)
+// EXP2: cout << fast_exp(x, fast_exp(y, p, MOD - 1), MOD)
+// FacNums = (x+1)(y+1)(z+1)...
+// FacSum = (a^0+a^1...+a^x)(b^0+...+b^y)
+// FacMul = N(x+1)(y+1)(z+1)/2
--- codebook/8_Tree/HLD.cpp
+++ codebook/8_Tree/HLD.cpp
@@ -13,11 +13,11 @@ struct HLD {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    void work(int root = 0) {
-        top[root] = root;
-        dep[root] = 0;
-        parent[root] = -1;
-        dfs1(root); dfs2(root);
+    void work(int rt = 0) {
+        top[rt] = rt;
+        dep[rt] = 0;
+        parent[rt] = -1;
+        dfs1(rt); dfs2(rt);
     }
     void dfs1(int u) {
         if (parent[u] != -1)
@@ -62,27 +62,23 @@ struct HLD {
         return seq[in[u] - dep[u] + d];
     }
     bool isAncester(int u, int v) {
-        // 判斷 u 是否是 v 的祖先
         return in[u] <= in[v] && in[v] < out[u];
     }
-    int rootedParent(int u, int v) {
-        // 根據新根節點 u 計算 v 的父節點
-        swap(u, v);
-        if (u == v) return u;
-        if (!isAncester(u, v)) return parent[u];
-        auto it = upper_bound(adj[u].begin(), adj[u].end(), v, [&](int x, int y) {
+    int rootedParent(int rt, int v) {
+        swap(rt, v);
+        if (rt == v) return rt;
+        if (!isAncester(rt, v)) return parent[rt];
+        auto it = upper_bound(adj[rt].begin(), adj[rt].end(), v, [&](int x, int y) {
             return in[x] < in[y];
         }) - 1;
         return *it;
     }
-    int rootedSize(int u, int v) {
-        // 根據新根節點 u 計算子樹 v 的大小
-        if (u == v) return n;
-        if (!isAncester(v, u)) return siz[v];
-        return n - siz[rootedParent(u, v)];
+    int rootedSize(int rt, int v) {
+        if (rt == v) return n;
+        if (!isAncester(v, rt)) return siz[v];
+        return n - siz[rootedParent(rt, v)];
     }
-    int rootedLca(int a, int b, int c) {
-        // 根據新的根節點計算三個節點 a、b 和 c 的最近公共祖先
-        return lca(a, b) ^ lca(b, c) ^ lca(c, a);
+    int rootedLca(int a, int b, int rt) {
+        return lca(a, b) ^ lca(b, rt) ^ lca(rt, a);
     }
 };
