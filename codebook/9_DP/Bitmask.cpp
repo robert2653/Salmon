94f3ad7c11e7a6d6e67a4e91310e8b75e3e8dcda
robert2653 <robert4812653@gmail.com>
2024-09-04 08:11:24 +0800

    Add more example to SOS and Bitmask

--- codebook/9_DP/Bitmask.cpp
+++ codebook/9_DP/Bitmask.cpp
@@ -1,47 +1,79 @@
-void hamiltonianPath(){
+void hamiltonianPath() {
     int n, m; cin >> n >> m;
-    vector adj(n, vector<int>());
+    vector<vector<int>> adj(n);
     for (int i = 0; i < m; i++) {
         int u, v; cin >> u >> v;
         adj[--v].push_back(--u);
     }
     // 以...為終點，走過...
-    vector dp(n, vector<int>(findBit(n)));
+    vector dp(n, vector<int>(1 << n));
     dp[0][1] = 1;
-    for (int mask = 1; mask < findBit(n); mask++) {
+    for (int mask = 1; mask < 1 << n; mask++) {
         if ((mask & 1) == 0) continue;
         for (int i = 0; i < n; i++) {
-            if ((mask & findBit(i)) == 0) continue;
-            if (i == n - 1 && mask != findBit(n) - 1) continue;
-            int pre_mask = mask ^ findBit(i);
+            if ((mask >> i & 1) == 0) continue;
+            if (i == n - 1 && mask != (1 << n) - 1) continue;
+            int pre = mask ^ (1 << i);
             for (int j : adj[i]) {
-                if ((pre_mask & findBit(j)) == 0) continue;
-                dp[i][mask] = (dp[i][mask] + dp[j][pre_mask]) % Mod;
+                if ((pre >> j & 1) == 0) continue;
+                dp[i][mask] = (dp[i][mask] + dp[j][pre]) % Mod;
             }
         }
     }
-    cout << dp[n - 1][findBit(n) - 1] << "\n";
+    cout << dp[n - 1][(1 << n) - 1] << "\n";
 }
 void elevatorRides() {
-    int n, x; cin >> n >> x; vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    vector<array<int, 2>> dp(findBit(n));
-    dp[0][0] = 1; // 次數、已使用人數
-    for (int mask = 1; mask < findBit(n); mask++) {
-        dp[mask][0] = dp[mask][1] = 2e9;
+    int n, x; cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int> dp(1 << n), f(1 << n);
+    dp[0] = 1; // 次數、已使用人數
+    for (int mask = 1; mask < 1 << n; mask++) {
+        dp[mask] = 2E9;
         for (int i = 0; i < n; i++) {
-            if ((mask & findBit(i)) == 0) continue;
-            int pre_mask = mask ^ findBit(i);
-            if (dp[pre_mask][1] + a[i] <= x) {
-                if (dp[pre_mask][0] < dp[mask][0] || dp[pre_mask][0] == dp[mask][0] && dp[pre_mask][1] + a[i] < dp[mask][1]) {
-                    dp[mask][0] = dp[pre_mask][0];
-                    dp[mask][1] = dp[pre_mask][1] + a[i];
+            if ((mask >> i & 1) == 0) continue;
+            int pre = mask ^ (1 << i);
+            if (f[pre] + a[i] <= x) {
+                if (dp[pre] < dp[mask] || dp[pre] == dp[mask] && f[pre] + a[i] < f[mask]) {
+                    dp[mask] = dp[pre];
+                    f[mask] = f[pre] + a[i];
                 }
-            } else if (dp[pre_mask][0] + 1 < dp[mask][0] || dp[pre_mask][0] + 1 == dp[mask][0] && a[i] < dp[mask][1]) {
-                dp[mask][0] = dp[pre_mask][0] + 1;
-                dp[mask][1] = a[i];
+            } else if (dp[pre] + 1 < dp[mask] || dp[pre] + 1 == dp[mask] && a[i] < f[mask]) {
+                dp[mask] = dp[pre] + 1;
+                f[mask] = a[i];
             }
         }
     }
-    cout << dp[findBit(n) - 1][0] << "\n";
+    cout << dp[(1 << n) - 1] << "\n";
+}
+void minClique() { // 移掉一些邊, 讓整張圖由最少團組成
+    int n, m;
+    cin >> n >> m;
+    vector<bitset<N>> g(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        g[u][v] = g[v][u] = 1;
+    }
+    vector<int> dp(1 << n, inf);
+    dp[0] = 1;
+    for (int mask = 0; mask < 1 << n; mask++) { // 先正常 dp
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) {
+                int pre = mask ^ (1 << i);
+                if (dp[pre] == 1 && (g[i] & bitset<N>(pre)) == pre) {
+                    dp[mask] = 1; // i 有連到所有 pre
+                }
+            }
+        }
+    }
+    for (int mask = 0; mask < 1 << n; mask++) { // 然後枚舉子集 dp
+        for (int sub = mask; sub; --sub &= mask) {
+            dp[mask] = min(dp[mask], dp[sub] + dp[mask ^ sub]);
+        }
+    }
+    cout << dp[(1 << n) - 1] << "\n";
 }
--- codebook/9_DP/SOS.cpp
+++ codebook/9_DP/SOS.cpp
@@ -1,16 +1,16 @@
-// 使用情況: 跟 bit 與(被)包含有關, 且 x 在 1e6 左右
+// 使用情況: 跟 bit 與(被)包含有關, 且 x 在 1E6 左右
 // 題目: 一數組, 問有多少所有數 & 起來為 0 的集合數
-// dp[x]代表包含 x 的 y 個數(比x大且bit 1全包含 x 的有幾個)
-// 答案應該包含在 dp[0]内，但是有重複元素，所以考慮容斥
+// dp[x] 代表包含 x 的 y 個數(比 x 大且 bit 1 全包含 x 的有幾個)
+// 答案應該包含在 dp[0] 内，但是有重複元素，所以考慮容斥
 // => ans = \sum _{i=0}^{n} (-1)^{pop_count(i)} 2^{dp[i]-1}
-// => 全部為0的個數 − 至少一個為1的個數 + 至少兩個為1的個數
+// => 全部為 0 的個數 − 至少一個為 1 的個數 + 至少兩個為 1 的個數
 void solve() {
     int n; cin >> n; Z ans = 0;
     vector<int> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
     int m = __lg(*max_element(a.begin(), a.end())) + 1;
-    // 定義 dp[mask] 為 mask 被包含於 a[i] 的 a[i] 個數
+    // 定義 dp[mask] 為 mask 被包含於 a[i] 的 i 個數
     vector<Z> dp(1 << m);
     for (int i = 0; i < n; i++)
         dp[a[i]] += 1;
@@ -25,4 +25,37 @@ void solve() {
         ans += sgn * (power(Z(2), dp[mask].val()) - 1);
     }
     cout << ans << "\n";
+}
+
+// x | y = x, 代表包含於 x 的 y 個數, 定義為 dp[x][0]
+// x & y = x, 代表包含 x 的 y 個數, 定義為 dp[x][1]
+// x & y != 0, 代表至少有一個位元都為 1 的 y 個數, = n - 與自己相同 - ~dp[x][0]
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    int m = __lg(*max_element(a.begin(), a.end())) + 1;
+
+    vector<array<ll, 2>> dp(1 << m);
+    for (int i = 0; i < n; i++) {
+        dp[a[i]][0] += 1;
+        dp[a[i]][1] += 1;
+    }
+    
+    for (int i = 0; i < m; i++) {
+        for (int mask = 0; mask < 1 << m; mask++) {
+            if (mask >> i & 1) {
+                int pre = mask ^ (1 << i);
+                dp[mask][0] += dp[pre][0];
+                dp[pre][1] += dp[mask][1];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << dp[a[i]][0] << " " << dp[a[i]][1] << " " << n - (dp[((1 << m) - 1) ^ a[i]][0]) << "\n";
+    }
 }
