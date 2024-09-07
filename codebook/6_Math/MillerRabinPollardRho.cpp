29181c92af57030ac0877df999180ed2f3ce9efb
robert2653 <robert4812653@gmail.com>
2024-09-08 03:08:39 +0800

    Fix some bug

--- codebook/6_Math/MillerRabinPollardRho.cpp
+++ codebook/6_Math/MillerRabinPollardRho.cpp
@@ -18,7 +18,7 @@ vector<ll> chk {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 bool check(ll a, ll d, int s, ll n) {
     a = power(a, d, n);
     if (a <= 1) return 1;
-    for (int i = 0; i < s; ++i, a = mul(a, a, n)) {
+    for (int i = 0; i < s; i++, a = mul(a, a, n)) {
         if (a == 1) return 0;
         if (a == n - 1) return 1;
     }
@@ -29,8 +29,7 @@ bool IsPrime(ll n) {
     if (n % 2 == 0) return n == 2;
     ll d = n - 1, s = 0;
     while (d % 2 == 0) {
-        d *= 2;
-        s++;
+        d /= 2, s++;
     }
     for (ll i : chk) {
         if (!check(i, d, s, n)) return 0;
@@ -47,14 +46,13 @@ ll FindFactor(ll n) {
     auto f = [&](ll a) {
         return (mul(a, a, n) + t) % n;
     };
-    for (int l = 2; ; l <<= 1) {
+    for (int l = 2; ; l *= 2) {
         x = y;
         int m = min(l, 32);
         for (int i = 0; i < l; i += m) {
             d = 1;
             for (int j = 0; j < m; ++j) {
-                y = f(y);
-                d = mul(d, abs(x - y), n);
+                y = f(y), d = mul(d, abs(x - y), n);
             }
             ll g = gcd(d, n);
             if (g == n) {
