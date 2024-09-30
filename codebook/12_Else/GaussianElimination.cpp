4bab1a9e4270db693e8354ad728a690de959c1e0
robert2653 <robert4812653@gmail.com>
2024-09-30 20:46:23 +0800

    Add GaussianElimination.cpp and Modify Matrix.cpp to be compatible with it

---
+++ codebook/12_Else/GaussianElimination.cpp
@@ -0,0 +1,86 @@
+// 找反矩陣就開 2n，右邊放單位矩陣, 做完檢查左半是不是單位, 回傳右半
+// 0  : no solution
+// -1 : infinity solution
+// 1  : one solution
+template<class T>
+tuple<T, int, vector<T>> gaussianElimination(vector<vector<T>> a) {
+    int n = a.size(), m = a[0].size(), rk = 0, sgn = 1;
+    T det = 1;
+    bool zero_det = false;
+    for (int c = 0; c < n; c++) {
+        int p = -1;
+        for (int r = rk; r < n; r++) {
+            if (a[r][c] != 0) {
+                p = r;
+                break;
+            }
+        }
+        if (p == -1) {
+            zero_det = true;
+            continue;
+        }
+        if (p != rk) swap(a[rk], a[p]), sgn *= -1;
+        det *= a[rk][c];
+        T inv = 1 / a[rk][c];
+        for (int j = c; j < m; j++) a[rk][j] *= inv;
+        for (int r = 0; r < n; r++) {
+            if (r == rk || a[r][c] == 0) continue;
+            T fac = a[r][c];
+            for (int j = c; j < m; j++)
+                a[r][j] -= fac * a[rk][j];
+        }
+        rk++;
+    }
+    det = (zero_det ? 0 : det * sgn);
+    for (int r = rk; r < n; r++)
+        if (a[r][m - 1] != 0) return {det, 0, {}};
+    if (rk < n) return {det, -1, {}};
+    vector<T> ans(n);
+    for (int i = 0; i < n; i++) ans[i] = a[i][m - 1];
+    return {det, 1, ans};
+}
+template<class T>
+tuple<int, vector<T>, vector<vector<T>>> findBasis(vector<vector<T>> a) {
+    int n = a.size(), m = a[0].size(), rk = 0;
+    vector<int> pos(m - 1, -1);
+    for (int c = 0; c < m - 1; c++) {
+        int p = -1;
+        for (int r = rk; r < n; r++) {
+            if (a[r][c] != 0) {
+                p = r;
+                break;
+            }
+        }
+        if (p == -1) continue;
+        if (p != rk) swap(a[rk], a[p]);
+        pos[c] = rk;
+        T inv = 1 / a[rk][c];
+        for (int j = c; j < m; j++) a[rk][j] *= inv;
+        for (int r = 0; r < n; r++) {
+            if (r == rk || a[r][c] == 0) continue;
+            T fac = a[r][c];
+            for (int j = c; j < m; j++)
+                a[r][j] -= fac * a[rk][j];
+        }
+        rk++;
+    }
+    vector<T> sol(m - 1);
+    vector<vector<T>> basis;
+    for (int r = rk; r < n; r++)
+        if (a[r][m - 1] != 0)
+            return {-1, sol, basis};
+    for (int c = 0; c < m - 1; c++)
+        if (pos[c] != -1)
+            sol[c] = a[pos[c]][m - 1];
+    for (int c = 0; c < m - 1; c++)
+        if (pos[c] == -1)
+            vector<T> v(m - 1);
+            v[c] = 1;
+            for (int j = 0; j < m - 1; j++)
+                if (pos[j] != -1)
+                    v[j] = -a[pos[j]][c];
+            basis.push_back(v);
+    return {rk, sol, basis};
+}
+template<class T>
+using Matrix = vector<vector<T>>;
--- codebook/6_Math/Matrix.cpp
+++ codebook/6_Math/Matrix.cpp
@@ -1,45 +1,28 @@
 template<class T>
-struct Matrix {
-    int n, m;
-    vector<vector<T>> mat;
-    constexpr Matrix(int n_, int m_) { init(n_, m_); }
-    constexpr Matrix(vector<vector<T>> mat_) { init(mat_); }
-    constexpr void init(int n_, int m_) {
-        n = n_; m = m_;
-        mat.assign(n, vector<T>(m));
-    }
-    constexpr void init(vector<vector<T>> mat_) {
-        n = mat_.size();
-        m = mat_[0].size();
-        mat = mat_;
-    }
-    constexpr Matrix &operator*=(const Matrix &rhs) & {
-        assert(mat[0].size() == rhs.mat.size());
-        int n = mat.size(), k = mat[0].size(), m = rhs.mat[0].size();
-        Matrix res(n, m);
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                for (int l = 0; l < k; l++)
-                    res.mat[i][j] += mat[i][l] * rhs.mat[l][j];
-        mat = res.mat;
-        return *this;
-    }
-    friend constexpr Matrix operator*(Matrix lhs, const Matrix &rhs) {
-        return lhs *= rhs;
-    }
-};
-template<class T>
-constexpr Matrix<T> unit(int n) {
-    Matrix<T> res(n, n);
+vector<vector<T>> operator*(const vector<vector<T>> &a, const vector<vector<T>> &b) {
+    int n = a.size(), k = a[0].size(), m = b[0].size();
+    assert(k == b.size());
+    vector<vector<T>> res(n, vector<T>(m));
     for (int i = 0; i < n; i++)
-        res.mat[i][i] = 1;
+        for (int j = 0; j < m; j++)
+            for (int l = 0; l < k; l++)
+                res[i][j] += a[i][l] * b[l][j];
     return res;
 }
 template<class T>
-constexpr Matrix<T> power(Matrix<T> a, ll b) {
-    assert(a.n == a.m);
-    Matrix<T> res = unit<T>(a.n);
-    for (; b; b /= 2, a *= a)
-        if (b % 2) res *= a;
+vector<vector<T>> unit(int n) {
+    vector<vector<T>> res(n, vector<T>(n));
+    for (int i = 0; i < n; i++)
+        res[i][i] = 1;
     return res;
 }
+template<class T>
+vector<vector<T>> power(vector<vector<T>> a, ll b) {
+    int n = a.size();
+    assert(n == a[0].size());
+    auto res = unit<T>(n);
+    for (; b; b /= 2, a = a * a)
+        if (b % 2) res = res * a;
+    return res;
+}
+using Matrix = vector<vector<Z>>;
