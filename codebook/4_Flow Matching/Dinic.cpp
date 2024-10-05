f17a8c00e83cc64b7da948cbe1522443b5aa6dc5
robert2653 <robert4812653@gmail.com>
2024-10-05 23:57:20 +0800

    Shorten code length and remove MCMF dijkstra

--- codebook/4_Flow Matching/Dinic.cpp
+++ codebook/4_Flow Matching/Dinic.cpp
@@ -1,82 +1,82 @@
 template<class T>
 struct Dinic {
-    struct Edge {
+    struct _Edge {
         int to;
-        T flow, cap; // 流量跟容量
+        T f, cap; // 流量跟容量
     };
     int n, m, s, t;
-    const T INF_FlOW = 1 << 30;
-    vector<vector<int>> adj; // 此點對應的 edges 編號
-    vector<Edge> edges; // 幫每個 edge 編號
-    vector<int> dis, ptr;
+    const T INF_FlOW = 1LL << 60;
+    vector<vector<int>> g;
+    vector<_Edge> e;
+    vector<int> h, cur;
     Dinic(int n_ = 0) { init(n_); }
     void init(int n_) {
         n = n_; m = 0;
-        dis.resize(n); ptr.resize(n);
-        adj.assign(n, {});
-        edges.clear();
+        h.resize(n); cur.resize(n);
+        g.assign(n, {});
+        e.clear();
     }
     void add_edge(int u, int v, T cap) {
-        // 偶數 id 是正向邊
-        edges.push_back({v, 0, cap});
-        edges.push_back({u, 0, 0});
-        adj[u].push_back(m++);
-        adj[v].push_back(m++);
+        e.push_back({v, 0, cap});
+        e.push_back({u, 0, 0});
+        g[u].push_back(m++);
+        g[v].push_back(m++);
     }
     bool bfs() {
-        fill(dis.begin(), dis.end(), -1);
-        dis[s] = 0; queue<int> q;
+        fill(h.begin(), h.end(), -1);
+        h[s] = 0; queue<int> q;
         q.push(s);
-        while (!q.empty() && dis[t] == -1) {
+        while (!q.empty()) {
             int u = q.front(); q.pop();
-            for (int id : adj[u]) {
-                Edge &e = edges[id];
-                if (e.flow == e.cap) continue;
-                if (dis[e.to] == -1) {
-                    dis[e.to] = dis[u] + 1;
-                    q.push(e.to);
+            for (int id : g[u]) {
+                auto [v, f, cap] = e[id];
+                if (f == cap) continue;
+                if (h[v] == -1) {
+                    h[v] = h[u] + 1;
+                    if (v == t) return true;
+                    q.push(v);
                 }
             }
         }
-        return dis[t] != -1;
+        return false;
     }
     T dfs(int u, T flow) {
         if (flow == 0) return 0;
         if (u == t) return flow;
-        for (int &cur = ptr[u]; cur < adj[u].size(); cur++) {
-            Edge &e = edges[adj[u][cur]];
-            if (dis[u] + 1 != dis[e.to]) continue;
-            if (e.cap == e.flow) continue;
-            T mn = dfs(e.to, min(flow, e.cap - e.flow));
+        for (int &i = cur[u]; i < g[u].size(); i++) {
+            int j = g[u][i];
+            auto [v, f, cap] = e[j];
+            if (h[u] + 1 != h[v]) continue;
+            if (f == cap) continue;
+            T mn = dfs(v, min(flow, cap - f));
             if (mn > 0) {
-                e.flow += mn;
-                edges[adj[u][cur] ^ 1].flow -= mn;
+                e[j].f += mn;
+                e[j ^ 1].f -= mn;
                 return mn;
             }
         }
-        return 0;   // 到不了終點就會 return 0
+        return 0;
     }
     T work(int s_, int t_) {
-        s = s_; t = t_; T flow = 0;
+        s = s_; t = t_; T f = 0;
         while (bfs()) {
-            fill(ptr.begin(), ptr.end(), 0);
+            fill(cur.begin(), cur.end(), 0);
             while (true) {
                 T res = dfs(s, INF_FlOW);
                 if (res == 0) break;
-                flow += res;
+                f += res;
             }
         }
-        return flow;
+        return f;
     }
     void reset() {
-        for (int i = 0; i < m; i++)
-            edges[i].flow = 0;
+        for (int i = 0; i < m; i++) e[i].f = 0;
     }
-    void reuse(int n_) { // 走殘留網路, res += flow
+    void reuse(int n_) { // 走殘留網路, res += f
         while (n < n_) {
-            adj.emplace_back();
-            dis.emplace_back();
-            ptr.emplace_back();
+            g.emplace_back();
+            h.emplace_back();
+            cur.emplace_back();
             n += 1;
         }
     }
