5278c5d50039228967da84e113100c6e14066f11
robert2653 <robert4812653@gmail.com>
2024-10-08 04:59:10 +0800

    Shorten code len

--- codebook/6_Math/Miller_Rabin_Pollard_Rho.cpp
+++ codebook/6_Math/Miller_Rabin_Pollard_Rho.cpp
@@ -24,20 +24,16 @@ bool isPrime(ll n) {
     if (n < 2) return 0;
     if (n % 2 == 0) return n == 2;
     ll d = n - 1, s = 0;
-    while (d % 2 == 0) {
-        d /= 2, s++;
-    }
-    for (ll i : chk) {
+    while (d % 2 == 0) d /= 2, s++;
+    for (ll i : chk)
         if (!check(i, d, s, n)) return 0;
-    }
     return 1;
 }
 const vector<ll> small = {2, 3, 5, 7, 11, 13, 17, 19};
 ll findFactor(ll n) {
     if (isPrime(n)) return 1;
-    for (ll p : small) {
+    for (ll p : small)
         if (n % p == 0) return p;
-    }
     ll x, y = 2, d, t = 1;
     auto f = [&](ll a) {
         return (mul(a, a, n) + t) % n;
@@ -47,9 +43,8 @@ ll findFactor(ll n) {
         int m = min(l, 32);
         for (int i = 0; i < l; i += m) {
             d = 1;
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < m; j++)
                 y = f(y), d = mul(d, abs(x - y), n);
-            }
             ll g = __gcd(d, n);
             if (g == n) {
                 l = 1, y = 2, ++t;
