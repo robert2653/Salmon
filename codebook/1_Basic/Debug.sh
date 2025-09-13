e80c281bbdd8a1a45212d4d880b038fe1d3c1b4a
robert2653 <robert4812653@gmail.com>
2025-09-13 13:33:05 +0800

    add debug

---
+++ codebook/1_Basic/Debug.sh
@@ -0,0 +1,15 @@
+CODE1="a"
+CODE2="brute"
+set -e
+g++ $CODE1.cpp -o $CODE1
+g++ $CODE2.cpp -o $CODE2
+for ((i=0;;i++))
+do
+    echo "$i"
+    g++ gen.cpp -o gen
+    ./gen > input
+    # python3 gen.py > input
+    ./$CODE1 < input > $CODE1.out
+    ./$CODE2 < input > $CODE2.out
+    cmp $CODE1.out $CODE2.out || break
+done
