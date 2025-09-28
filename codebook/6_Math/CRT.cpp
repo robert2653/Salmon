b4839ecaaffe0644d8f5229251092c9cb90810a1
robert2653 <robert4812653@gmail.com>
2025-09-28 08:17:33 +0800

    Fix CRT

--- codebook/6_Math/CRT.cpp
+++ codebook/6_Math/CRT.cpp
@@ -1,28 +1,26 @@
-ll exgcd(ll a, ll b, ll &x, ll &y) {
-    if (!b) {
-        x = 1, y = 0;
-        return a;
-    }
-    ll g = exgcd(b, a % b, y, x);
-    y -= a / b * x;
-    return g;
+// a * p.first + b * p.second = gcd(a, b)
+pair<ll, ll> exgcd(ll a, ll b) {
+    if (b == 0) return {1, 0};
+    auto [y, x] = exgcd(b, a % b);
+    return {x, y - (a / b) * x};
+}    
+// smallest non-negative solution
+using i128 = __int128_t;
+pair<ll, ll> CRT(ll r1, ll m1, ll r2, ll m2) {
+    ll g = __gcd(m1, m2);
+    if ((r2 - r1) % g) return {-1, g};
+    m1 /= g, m2 /= g;
+    auto [p1, p2] = exgcd(m1, m2);
+    i128 lcm = i128(m1) * m2 * g;
+    i128 res = i128(p1) * (r2 - r1) * m1 + r1;
+    return {(res % lcm + lcm) % lcm, lcm};
 }
-ll inv(ll x, ll m) {
-    ll a, b;
-    exgcd(x, m, a, b);
-    a %= m;
-    if (a < 0) a += m;
-    return a;
-}
-// gcd(mod) = 1, res % mod_i = remain_i
-// a: remain, mod
-ll CRT(vector<pair<ll, ll>> &a) {
-    ll s = 1, res = 0;
-    for (auto [r, m] : a) s *= m;
+ll EXCRT(vector<pair<ll, ll>> a) {
+    ll R = 0, M = 1;
     for (auto [r, m] : a) {
-        ll t = s / m;
-        res += r * t % s * inv(t, m) % s;
-        if (res >= s) res -= s;
+        auto [res, lcm] = CRT(R, M, r, m);
+        if (res == -1) return -1;
+        R = res, M = lcm;
     }
-    return res;
+    return R;
 }
