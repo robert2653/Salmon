680764bcb91d202b957c30cd2c6e1c4e4e196610
robert2653 <robert4812653@gmail.com>
2025-10-10 23:28:33 +0800

    Use sieve instead of init

--- codebook/6_Math/mobius.cpp
+++ codebook/6_Math/mobius.cpp
@@ -1,30 +1,11 @@
-const int N = 2E5;
-ll pref[N];
-void init() {
-    pref[1] = 1;
-    vector<ll> wei(N);  // wei = 0 代表是質數，-1 代表可被平方數整除
-    for (ll i = 2; i < N; i++) {
-        if (wei[i] == -1) {
-            pref[i] = pref[i - 1];
-            continue; // 包含平方
-        }
-        if (wei[i] == 0) {
-            wei[i] = 1;
-            for (ll j = 2; i * j < N; j++) {
-                if (j % i == 0) wei[i * j] = -1;
-                else if (wei[i * j] != -1) wei[i * j]++;
-            }
-        }
-        pref[i] = pref[i - 1] + (wei[i] % 2 == 0 ? 1 : -1);
-    }
-}
-void solve() {
+void solve() { // pref: pref of mu
     ll a, b, c, d, k; cin >> a >> b >> c >> d >> k;
+    sieve(N);
     auto cal = [&](ll x, ll y) -> int {
         int res = 0;
         for (int l = 1, r; l <= min(x, y); l = r + 1) {
             r = min(x / (x / l), y / (y / l));
-            res += (pref[r] - pref[l - 1]) * (x / l) * (y / l);  // 代推出來的式子
+            res += (pref[r] - pref[l - 1]) * (x / l) * (y / l);
         }
         return res;
     };
