66a1ca5449d7d91a059c852461743f82c52a7492
robert2653 <robert4812653@gmail.com>
2025-10-18 21:39:20 +0800

    Shorten length

--- codebook/6_Math/Mint.cpp
+++ codebook/6_Math/Mint.cpp
@@ -1,24 +1,19 @@
-ll mul(ll a, ll b, ll p) {
+ll mul(ll a, ll b, ll p) { // P 超過 int 再用，慢
     ll res = a * b - ll(1.L * a * b / p) * p;
     res %= p;
     if (res < 0) res += p;
     return res;
 }
-// 改 MLong: getMod() < (1ULL << 31)，會爆用 mul
-template<class T>
-constexpr T power(T a, ll b) {
+template<class T> constexpr T power(T a, ll b) {
     T res {1};
     for (; b > 0; b >>= 1, a = a * a)
-        if (b & 1) res = res * a;
+    if (b & 1) res = res * a;
     return res;
 }
-template<int P>
-struct Mint {
+template<int P> struct Mint {
     static int Mod;
-    static int getMod()
-    { return P > 0 ? P : Mod; }
-    static void setMod(int Mod_)
-    { Mod = Mod_; }
+    static int getMod() { return P > 0 ? P : Mod; }
+    static void setMod(int Mod_) { Mod = Mod_; }
     ll x;
     Mint(ll x = 0) : x {norm(x % getMod())} {}
     ll norm(ll x) const {
@@ -27,24 +22,17 @@ struct Mint {
         return x;
     }
     explicit operator int() const { return x; }
-    Mint operator-() const
-    { return Mint(norm(getMod() - x)); }
-    Mint inv() const
-    { return power(*this, getMod() - 2); }
-    Mint operator+(Mint rhs) const
-    { return Mint(norm(x + rhs.x)); }
-    Mint operator-(Mint rhs) const
-    { return Mint(norm(x - rhs.x)); }
-    Mint operator*(Mint rhs) const
-    { return Mint(mul(x, rhs.x, getMod())); }
-    Mint operator/(Mint rhs) const
-    { return *this * rhs.inv(); }
-
+    Mint operator-() const { return getMod() - x; }
+    Mint inv() const { return power(*this, getMod() - 2); }
+    Mint operator+(Mint a) const { return x + a.x; }
+    Mint operator-(Mint a) const { return x - a.x; }
+    Mint operator*(Mint a) const { return x * a.x; }
+    Mint operator/(Mint a) const { return *this * a.inv(); }
     
-    Mint &operator+=(Mint rhs) { return *this = *this + rhs; }
-    Mint &operator-=(Mint rhs) { return *this = *this - rhs; }
-    Mint &operator*=(Mint rhs) { return *this = *this * rhs; }
-    Mint &operator/=(Mint rhs) { return *this = *this / rhs; }
+    Mint &operator+=(Mint a) { return *this = *this + a; }
+    Mint &operator-=(Mint a) { return *this = *this - a; }
+    Mint &operator*=(Mint a) { return *this = *this * a; }
+    Mint &operator/=(Mint a) { return *this = *this / a; }
     
     friend istream &operator>>(istream &is, Mint &a)
     { ll v; is >> v; a = Mint(v); return is; }
@@ -53,7 +41,6 @@ struct Mint {
     bool operator==(Mint y) const { return x == y.x; }
     bool operator!=(Mint y) const { return x != y.x; }
 };
-template<>
-int Mint<0>::Mod = 998244353;
+template<> int Mint<0>::Mod = 998244353;
 constexpr int P = 1E9 + 7;
 using Z = Mint<P>;
