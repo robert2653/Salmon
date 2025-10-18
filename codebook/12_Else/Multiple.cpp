ed17d418418c0a6791988ecdfac82959f8e726c6
robert2653 <robert4812653@gmail.com>
2025-10-18 21:38:38 +0800

    Fix more readible

--- codebook/12_Else/Multiple.cpp
+++ codebook/12_Else/Multiple.cpp
@@ -1,45 +1,34 @@
 // Require:
 // Mint, NTT ~constructor and * operator
-const int P1 = 1045430273;
-const int P2 = 1051721729;
-const int P3 = 1053818881;
-const int r12 = Mint<P2>(Mint<P1>::getMod()).inv().x;
-const int r13 = Mint<P3>(Mint<P1>::getMod()).inv().x;
-const int r23 = Mint<P3>(Mint<P2>::getMod()).inv().x;
-const int r1323 = Mint<P3>(ll(r13) * r23).x;
-const ll w1 = Mint<P1>::getMod();
-const ll w2 = w1 * Mint<P2>::getMod();
-// Garner's Algorithm
-template <typename T>
-vector<T> arbitraryMult(const vector<int> &a, const vector<int> &b) {
+using i128 = __int128_t;
+const int P1 = 1045430273; // 2^20 * 997 + 1
+const int P2 = 1051721729; // 2^20 * 1003 + 1
+const int P3 = 1053818881; // 2^20 * 1007 + 1
+using Poly1 = Poly<1045430273, 3>;
+using Poly2 = Poly<1051721729, 6>;
+using Poly3 = Poly<1053818881, 7>;
+const i128 T1 = i128(P2) * P3;
+const i128 T2 = i128(P1) * P3;
+const i128 T3 = i128(P1) * P2;
+const int I1 = Mint<P1>(T1).inv().x;
+const int I2 = Mint<P2>(T2).inv().x;
+const int I3 = Mint<P3>(T3).inv().x;
+const i128 M = i128(P1) * P2 * P3;
+vector<i128> arbitraryMult(const vector<int> &a, const vector<int> &b) {
     int n = a.size(), m = b.size();
-    Poly<P1> x = Poly<P1>(a.begin(), a.end()) * Poly<P1>(b.begin(), b.end());
-    Poly<P2> y = Poly<P2>(a.begin(), a.end()) * Poly<P2>(b.begin(), b.end());
-    Poly<P3> z = Poly<P3>(a.begin(), a.end()) * Poly<P3>(b.begin(), b.end());
-    vector<T> res(x.size());
-    for (int i = 0; i < x.size(); i++) {
-        ll p = x[i].x;
-        ll q = (y[i].x + P2 - p) * r12 % P2;
-        ll r = ((z[i] + P3 - p) * r1323 + (P3 - q) * r23).x % P3;
-        res[i] = (T(r) * w2 + q * w1 + p);
-    }
+    Poly1 x = Poly1(a.begin(), a.end()) * Poly1(b.begin(), b.end());
+    Poly2 y = Poly2(a.begin(), a.end()) * Poly2(b.begin(), b.end());
+    Poly3 z = Poly3(a.begin(), a.end()) * Poly3(b.begin(), b.end());
+    vector<i128> res(x.size());
+    for (int i = 0; i < x.size(); i++)
+        res[i] = (x[i].x * T1 % M * I1 +
+                  y[i].x * T2 % M * I2 +
+                  z[i].x * T3 % M * I3) % M;
     return res;
 }
-private:
-    vector<int> Multiple(vector<int> a, vector<int> b) {
-        return norm(arbitraryMult<u128>(a, b));
-    }
-    vector<int> smallMul(vector<int> a, int v) {
-        vector<ll> res(a.begin(), a.end());
-        for (auto &x : res) x *= v;
-        return norm(res);
-    }
 public:
-    Bigint &operator*=(const Bigint &rhs) & {
-        x = rhs.size() == 1 ? smallMul(x, rhs.x[0]) : Multiple(x, rhs.x);
-        sgn *= rhs.sgn, resign();
-        return *this;
-    }
-    friend Bigint operator*(Bigint lhs, Bigint rhs) {
-        return lhs *= rhs;
+    friend Bigint operator*(Bigint a, Bigint b) {
+        a.x = a.norm(arbitraryMult(a.x, b.x));
+        a.sgn *= b.sgn, a.resign();
+        return a;
     }
