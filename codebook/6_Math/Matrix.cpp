e066452ee10b8735af78ec49832a85af31a70d13
robert2653 <robert4812653@gmail.com>
2025-10-20 19:36:01 +0800

    Shorten code length

--- codebook/6_Math/Matrix.cpp
+++ codebook/6_Math/Matrix.cpp
@@ -1,27 +1,19 @@
-template<class T>
-vector<vector<T>> operator*(const vector<vector<T>> &a, const vector<vector<T>> &b) {
+using Matrix = vector<vector<Z>>;
+Matrix operator*(const Matrix &a, const Matrix &b) {
     int n = a.size(), k = a[0].size(), m = b[0].size();
     assert(k == b.size());
-    vector<vector<T>> res(n, vector<T>(m));
+    Matrix res(n, vector<Z>(m));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             for (int l = 0; l < k; l++)
                 res[i][j] += a[i][l] * b[l][j];
     return res;
 }
-template<class T>
-vector<vector<T>> unit(int n) {
-    vector<vector<T>> res(n, vector<T>(n));
-    for (int i = 0; i < n; i++) res[i][i] = 1;
-    return res;
-}
-template<class T>
-vector<vector<T>> power(vector<vector<T>> a, ll b) {
+Matrix power(Matrix a, ll b) {
     int n = a.size();
-    assert(n == a[0].size());
-    auto res = unit<T>(n);
-    for (; b; b /= 2, a = a * a)
-        if (b % 2) res = res * a;
+    Matrix res(n, vector<Z>(n));
+    for (int i = 0; i < n; i++) res[i][i] = 1;
+    for (; b > 0; b >>= 1, a = a * a)
+        if (b & 1) res = res * a;
     return res;
 }
-using Matrix = vector<vector<Z>>;
