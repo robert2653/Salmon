fb88fa475578921a1b2b2eedf9e68bf20efd06ad
robert2653 <robert4812653@gmail.com>
2025-11-23 02:58:23 +0800

    Debug

--- codebook/9_DP/Projects.cpp
+++ codebook/9_DP/Projects.cpp
@@ -1,30 +1,29 @@
 void projects() { // 排程有權重問題，輸出價值最多且時間最少
-    struct E { int from, to, w, id; };
-    int n; cin >> n; vector<E> a(n + 1);
+    int n; cin >> n;
+    vector<array<int, 4>> a(n + 1);
     for (int i = 1; i <= n; i++) {
         int u, v, w; cin >> u >> v >> w;
-        a[i] = {u, v, w, i};
+        a[i] = {v, u, w, i};
     }
-    vector<array<ll, 2>> dp(n + 1); // w, time
-    vector<array<int, 2>> rec(n + 1); // 有沒選，上個是誰
     sort(a.begin(), a.end());
+    vector<array<ll, 4>> dp(n + 1);
+    // {w, time, 有沒選, 上個是誰}
+    vector<ll> vs(n + 1);
+    for (int i = 1; i <= n; i++) vs[i] = a[i][0];
     for (int i = 1; i <= n; i++) {
-        int id = prev(lower_bound(all(a), {0, a[i].from}, [](E x, E y) {
-            return x.to < y.to;
-        })) - a.begin();
+        int id = --lower_bound(all(vs), a[i][1]) - vs.begin();
         dp[i] = dp[i - 1];
-        ll nw = dp[id][0] + a[i].w;
-        ll nt = dp[id][1] + a[i].to - a[i].from;
+        ll nw = dp[id][0] + a[i][2];
+        ll nt = dp[id][1] + a[i][0] - a[i][1];
         if (dp[i][0] < nw || dp[i][0] == nw && dp[i][1] > nt) {
-            dp[i] = {nw, nt};
-            rec[i] = {1, id};
+            dp[i] = {nw, nt, 1, id};
         }
     }
     vector<int> ans;
     for (int i = n; i != 0;) {
-        if (rec[i][0]) {
-            ans.push_back(a[i].id);
-            i = rec[i][1];
+        if (dp[i][2]) {
+            ans.push_back(a[i][3]);
+            i = dp[i][3];
         } else i--;
-    }
+    } reverse(ans.begin(), ans.end());
 }
