fc6a1f7b87c6ed638577e055432016fe4bd09ca1
robert2653 <robert4812653@gmail.com>
2026-01-19 23:03:49 +0800

    Remove Mod and add rng

--- codebook/1_Basic/default_code.cpp
+++ codebook/1_Basic/default_code.cpp
@@ -3,18 +3,9 @@
 using namespace std;
 using ll = long long;
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 template<class T> void chmin(T &a, const T &b) { if (a > b) a = b; }
 template<class T> void chmax(T &a, const T &b) { if (a < b) a = b; }
-const int Mod = 1E9 + 7;
-int add(int a, int b) { a += b; if (a >= Mod) a -= Mod; return a; }
-int sub(int a, int b) { a -= b; if (a < 0) a += Mod; return a; }
-int mul(int a, int b) { return 1LL * a * b % Mod; }
-int power(int a, ll b) {
-    int ans = 1;
-    for (; b > 0; b >>= 1, a = mul(a, a))
-        if (b & 1) ans = mul(ans, a);
-    return ans;
-}
 
 void solve() {
 
@@ -23,13 +14,17 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     auto s = chrono::high_resolution_clock::now();
+
     int t = 1;
     cin >> t;
     while (t--) {
         solve();
     }
+
     auto e = chrono::high_resolution_clock::now();
     cerr << chrono::duration_cast<chrono::milliseconds>(e - s).count() << " ms\n";
+    
     return 0;
 }
