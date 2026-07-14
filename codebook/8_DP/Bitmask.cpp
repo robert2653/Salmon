5f20cd8e1762cdeb204135eaff7d9a5863c50d76
robert2653 <robert4812653@gmail.com>
2026-07-14 21:10:01 +0800

    Delete elevator

--- codebook/8_DP/Bitmask.cpp
+++ codebook/8_DP/Bitmask.cpp
@@ -22,30 +22,6 @@ void hamiltonianPath() {
     }
     cout << dp[n - 1][(1 << n) - 1] << "\n";
 }
-void elevatorRides() {
-    int n, x; cin >> n >> x;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    vector<int> dp(1 << n), f(1 << n);
-    dp[0] = 1; // 次數、已使用人數
-    for (int mask = 1; mask < 1 << n; mask++) {
-        dp[mask] = 2E9;
-        for (int i = 0; i < n; i++) {
-            if ((mask >> i & 1) == 0) continue;
-            int pre = mask ^ (1 << i);
-            if (f[pre] + a[i] <= x) {
-                if (dp[pre] < dp[mask] || dp[pre] == dp[mask] && f[pre] + a[i] < f[mask]) {
-                    dp[mask] = dp[pre];
-                    f[mask] = f[pre] + a[i];
-                }
-            } else if (dp[pre] + 1 < dp[mask] || dp[pre] + 1 == dp[mask] && a[i] < f[mask]) {
-                dp[mask] = dp[pre] + 1;
-                f[mask] = a[i];
-            }
-        }
-    }
-    cout << dp[(1 << n) - 1] << "\n";
-}
 void minClique() { // 移掉一些邊, 讓整張圖由最少團組成
     int n, m;
     cin >> n >> m;
