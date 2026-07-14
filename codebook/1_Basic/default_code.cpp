6dc2471d22cca5a894c74b310e352e8fa0f1a39e
robert2653 <robert4812653@gmail.com>
2026-07-14 21:10:16 +0800

    Update

--- codebook/1_Basic/default_code.cpp
+++ codebook/1_Basic/default_code.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 using ll = long long;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+namespace rgs = ranges;
+
 template<class T> void chmin(T &a, const T &b) { if (a > b) a = b; }
 template<class T> void chmax(T &a, const T &b) { if (a < b) a = b; }
 
@@ -15,16 +16,12 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    auto s = chrono::high_resolution_clock::now();
-
     int t = 1;
     cin >> t;
     while (t--) {
         solve();
     }
 
-    auto e = chrono::high_resolution_clock::now();
-    cerr << chrono::duration_cast<chrono::milliseconds>(e - s).count() << " ms\n";
     
     return 0;
 }
